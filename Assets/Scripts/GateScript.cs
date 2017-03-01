a54ea545bec016f9b9f8e2cc1d024a141b28f617
Sahir Boghani <sahirboghani@gmail.com>
2017-03-01 18:39:20 -0500

    Grave/Gate prefabs

---
+++ Assets/Scripts/GateScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateScript : MonoBehaviour {
+
+    public int requiredSkeletons = 10;
+
+    public void OpenGate(int currentSkeletons)
+    {
+        if (currentSkeletons >= requiredSkeletons)
+            gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/GraveScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraveScript : MonoBehaviour {
+
+    public Sprite skeletonSprite;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void raiseGrave()
+    {
+        spriteRenderer.sprite = skeletonSprite;
+    }
+}
