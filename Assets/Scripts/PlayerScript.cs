87a138d5df411978b33683b2a181544efb9569e0
Sahir Boghani <sahirboghani@gmail.com>
2017-03-07 19:51:39 -0500

    Setup scene with player

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+
+        float moveHor = Input.GetAxis("Horizontal");
+        float moveVer = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHor *speed, moveVer*speed);
+        rb2d.velocity = movement;
+    }
+}
